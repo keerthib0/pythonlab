52ed4c98090023d1168f5a67e2b94e7fb50f19ae
Keerthana Bainagari <keerthibynagari07@gmail.com>
2025-03-28 15:56:10 +0530

    Update prime.py

--- week1/prime.py
+++ week1/prime.py
@@ -1,9 +1,11 @@
 Prime Number python code:-
 
 n=int(input("enter a number:-"))
+flag=0
 for i in range(2,n):
       if(n%i==0):
-       print("not a prime number")
-       break;
-else:
+          flag=1
+          print("not a prime number")
+          break;
+if(flag==0):
      print("is a prime number")
