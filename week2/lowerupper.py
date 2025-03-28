cbd419d9ee8382cd7a3526ce9b815171026bc7e1
Keerthana Bainagari <keerthibynagari07@gmail.com>
2025-03-28 15:04:47 +0530

    Update lowerupper.py

--- week2/lowerupper.py
+++ week2/lowerupper.py
@@ -2,5 +2,9 @@ n=input("enter a character:-")[0]
 print(n)
 if(n.islower()):
      print(n,' is lowercase')
-else:
+elif(n.isupper()):
      print(n,'is uppercase')
+elif(n.isdigit()):
+     print(n,' is a digit')
+else:
+     print(n,' is a special character')
