79445385629f70fe733617268cdb38b10ce7aae5
Keerthana Bainagari <keerthibynagari07@gmail.com>
2025-03-28 16:08:55 +0530

    Update prime.py

--- week1/prime.py
+++ week1/prime.py
@@ -2,7 +2,7 @@ Prime Number python code:-
 
 n=int(input("enter a number:-"))
 flag=0
-for i in range(2,n):
+for i in range(2,n): //2 because its the smallest prime number//
       if(n%i==0):
           flag=1
           print("not a prime number")
